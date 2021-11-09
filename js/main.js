b7ba43456ae288fa25769d10c336a932c90967d6
NoyShabo <75336643+NoyShabo@users.noreply.github.com>
2021-11-09 14:10:02 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ console.log('hi');
 import './cmp/weather.cmp.js';
 import './cmp/count-down.cmp.js';
 import './cmp/users.cmp.js';
-// import './cmp/show-cords.cmp.js' // github pages don't let show coords
 
 const options = {
     el: '#app',
@@ -12,7 +11,6 @@ const options = {
         <hr>
         <count-down v-on:due="timerFinish"></count-down>
         <users></users>
-        <!--  <show-cords></show-cords> -->
     </section>
 
     `,
